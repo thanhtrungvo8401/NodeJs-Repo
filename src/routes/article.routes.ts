a5a5e9fce0044f1a66bab7f98ba80686e802b2e2
Trung Vo <trungvo@MacBook-Pro-2.local>
2022-06-05 15:56:55 +0700

    master: allow get article detail by slug

--- src/routes/article.routes.ts
+++ src/routes/article.routes.ts
@@ -4,60 +4,61 @@ import { Service } from "typedi";
 import { Article } from "../entities/article.entity";
 import { Route } from "../types";
 
-
 const createHandlers = [
-    async (req: express.Request, res: express.Response) => {
-        const article = new Article(req.body);
-        
-        article.createdAt = new Date();
+  async (req: express.Request, res: express.Response) => {
+    const article = new Article(req.body);
 
-        await article.save();
+    article.createdAt = new Date();
 
-        res.json({ success: true, data: article });
-    }
-]
+    await article.save();
+
+    res.json({ success: true, data: article });
+  },
+];
 
 const getDetailHandlers = [
-    async (req: express.Request, res: express.Response) => {
-        const id = req.params?.id;
+  async (req: express.Request, res: express.Response) => {
+    const slug = req.params?.slug;
 
-        const article = await Article.findOne({ _id: id });
-        
-        res.json({ success: true, data: article })
-    }
-]
+    const article = await Article.findOne({ slug: slug });
+
+    res.json({ success: true, data: article });
+  },
+];
 
 const getAllHandlers = [
-    async (req: express.Request, res: express.Response) => {
-        const articles = await Article.find({});
-        
-        res.json({ success: true, data: articles })
-    }
-]
+  async (req: express.Request, res: express.Response) => {
+    const articles = await Article.find({});
+
+    res.json({ success: true, data: articles });
+  },
+];
 
 const updateHandlers = [
-    async (req: express.Request, res: express.Response) => {
-        const body = req.body;
+  async (req: express.Request, res: express.Response) => {
+    const body = req.body;
 
-        if (body.title) {
-            body.slug = slugify(body.title, { lower: true, strict: true })
-        }
-    },
-    async (req: express.Request, res: express.Response) => {
-        const id = req.params?.id;
-
-        const article = await Article.findOneAndUpdate({ _id: id }, req.body, { runValidators: true, new: true })
-        res.json({ success: true, data: article })
+    if (body.title) {
+      body.slug = slugify(body.title, { lower: true, strict: true });
     }
-]
+  },
+  async (req: express.Request, res: express.Response) => {
+    const id = req.params?.id;
 
+    const article = await Article.findOneAndUpdate({ _id: id }, req.body, {
+      runValidators: true,
+      new: true,
+    });
+    res.json({ success: true, data: article });
+  },
+];
 
 @Service()
 export class ArticleRoutes extends Route {
-    configuration(): void {
-        this.get('/', ...getAllHandlers);
-        this.get('/:id', ...getDetailHandlers);
-        this.post('/', ...createHandlers);
-        this.put('/:id', ...updateHandlers);
-    }
+  configuration(): void {
+    this.get("/", ...getAllHandlers);
+    this.get("/:slug", ...getDetailHandlers);
+    this.post("/", ...createHandlers);
+    this.put("/:id", ...updateHandlers);
+  }
 }
