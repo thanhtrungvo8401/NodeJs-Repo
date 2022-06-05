f6ec2d4115947b92ff2d67288740e4b4ec65acdd
Trung Vo <trungvo@MacBook-Pro-2.local>
2022-06-05 14:57:24 +0700

    master: update to call to mongodb atlas

--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,82 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 import express from "express";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import cors from "cors";
 
 // Services:
 import { DBMongo } from "./services/db-mongo";
 
 // Routes:
-import { RoutesConfig } from './routes';
+import { RoutesConfig } from "./routes";
 
 // Types:
-import { ApplicationOptions, AppService } from './types';
-import Container from 'typedi';
+import { ApplicationOptions, AppService } from "./types";
+import Container from "typedi";
 
 class Application {
-    private port: Number;
-    private services: Function[];
+  private port: Number;
+  private services: Function[];
 
-    constructor(private app: express.Express, private options: ApplicationOptions) {
-        console.group('=======================================')
-        console.log("APPLICATION start installing\n")
-        this.port = this.options.port;
-        this.services = this.options.services;
+  constructor(
+    private app: express.Express,
+    private options: ApplicationOptions
+  ) {
+    console.group("=======================================");
+    console.log("APPLICATION start installing\n");
+    this.port = this.options.port;
+    this.services = this.options.services;
+  }
+
+  async start() {
+    await this.installService();
+
+    await this.installAppMiddleware();
+
+    await this.installRoutes();
+
+    await this.installErrorHandler();
+
+    this.app.listen(this.port, () => {
+      console.log(`\nApplication successfully started at port: ${this.port}`);
+      console.log("=======================================");
+      console.groupEnd();
+    });
+  }
+
+  private async installService() {
+    for (const Service of this.services) {
+      await (Container.get(Service) as AppService).install();
+      console.log(`${Service.name} installed successfully`);
     }
+  }
 
-    async start() {
-        await this.installService();
+  private async installRoutes() {
+    RoutesConfig.config(this.app);
+  }
 
-        await this.installAppMiddleware();
+  private async installAppMiddleware() {
+    this.app.use(bodyParser.json());
 
-        await this.installRoutes();
+    this.app.use(
+      morgan(":method :url :status :res[content-length] - :response-time ms")
+    );
 
-        await this.installErrorHandler();
+    this.app.use(cors());
+  }
 
-        this.app.listen(this.port, () => {
-            console.log(`\nApplication successfully started at port: ${this.port}`);
-            console.log('=======================================')
-            console.groupEnd()
-        })
-    }
-
-    private async installService() {
-        for (const Service of this.services) {
-            await (Container.get(Service) as AppService).install();
-            console.log(`${Service.name} installed successfully`);
-        }
-    }
-
-    private async installRoutes() {
-        RoutesConfig.config(this.app);
-    }
-
-    private async installAppMiddleware() {
-        this.app.use(bodyParser.json());
-    }
-
-    private async installErrorHandler() {
-        this.app.use((err: any, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
-            res.json({ success: false, err });
-        })
-    }
+  private async installErrorHandler() {
+    this.app.use(
+      (
+        err: any,
+        _req: express.Request,
+        res: express.Response,
+        _next: express.NextFunction
+      ) => {
+        res.json({ success: false, err });
+      }
+    );
+  }
 }
 
 const app = new Application(express(), { port: 8080, services: [DBMongo] });
--- src/configs/db-mongo.ts
+++ src/configs/db-mongo.ts
@@ -1,3 +1,3 @@
 export const dbMongo = {
-    url: 'mongodb://localhost:27017/my-database'
-}
+  url: "mongodb+srv://trungvt:6EUooMwUVZHWG8l0@cluster0.csph5.mongodb.net/blog-database?retryWrites=true&w=majority",
+};
